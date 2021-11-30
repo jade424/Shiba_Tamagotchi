dae35b342fdedf7ff5fc64f6ab12ad0ec83fbd83
jade424 <jade424.tn@gmail.com>
2021-11-30 12:46:21 -0500

    updating

--- script/script.js
+++ script/script.js
@@ -1,41 +1,42 @@
-//function for when "let's play" is selected
-// let petName = document.getElementById('petname');
-// console.log(petname);
-// const heading = document.getElementById('heading');
-// console.log(heading);
-// First - need to create DOM for when button "let's Play" is clicked current Shiba image will update
+// //function for when "let's play" is selected
+// // let petName = document.getElementById('petname');
+// // console.log(petname);
+// // const heading = document.getElementById('heading');
+// // console.log(heading);
+// // First - need to create DOM for when button "let's Play" is clicked current Shiba image will update
 
-class shibaGotchi {
-    constructor(name) {
-        this.name = name;
-        this.hunger = 0;
-        this.boredom = 0;
-        this.affection = 10;
-        this.tired = 0;
-        this.age = 0; 
-        this.alive = "";
-    }
-}
+// class shibaGotchi {
+//     constructor(name) {
+//         this.name = name;
+//         this.hunger = 0;
+//         this.boredom = 0;
+//         this.affection = 10;
+//         this.tired = 0;
+//         this.age = 0; 
+//         this.alive = "";
+//     }
+// }
 
-//functions
+// //functions
 
-petName() {
+function petName() {
 
-}
+// }
 
-$("#attribute-area").toggle();
-$("#name-area").toggle();
-$("#button-area").toggle();
-$("#pet-area").toggle();
+// $("#attribute-area").toggle();
+// $("#name-area").toggle();
+// $("#button-area").toggle();
+// $("#pet-area").toggle();
 
-//clicking buttons    
+// //clicking buttons    
 
 const button = document.querySelector('button');
 
- button.addEventListener("click", petName()) {
+button.addEventListener("click", petName()) {
      const input = document.querySelector('input');
      const title = document.querySelector('title');
      title.appendChild
      title.textcontent = input.value;
-}
-petName();
+ 
+ petName();
+
