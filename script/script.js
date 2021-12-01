68429fc80bbe3b200a8590c0eb7f3efe9458c21e
jade424 <jade424.tn@gmail.com>
2021-12-01 18:18:41 -0500

    oof

--- script/script.js
+++ script/script.js
@@ -1,42 +1,7 @@
-// //function for when "let's play" is selected
-// // let petName = document.getElementById('petname');
-// // console.log(petname);
-// // const heading = document.getElementById('heading');
-// // console.log(heading);
-// // First - need to create DOM for when button "let's Play" is clicked current Shiba image will update
-
-// class shibaGotchi {
-//     constructor(name) {
-//         this.name = name;
-//         this.hunger = 0;
-//         this.boredom = 0;
-//         this.affection = 10;
-//         this.tired = 0;
-//         this.age = 0; 
-//         this.alive = "";
-//     }
-// }
-
-// //functions
-
-function petName() {
-
-// }
-
-// $("#attribute-area").toggle();
-// $("#name-area").toggle();
-// $("#button-area").toggle();
-// $("#pet-area").toggle();
-
-// //clicking buttons    
-
-const button = document.querySelector('button');
-
-button.addEventListener("click", petName()) {
-     const input = document.querySelector('input');
-     const title = document.querySelector('title');
-     title.appendChild
-     title.textcontent = input.value;
- 
- petName();
+//game set up
+//click interactivity
+//Pet
+//Food Pebbles
+//Heart bubbles
+//Rest Button
 
