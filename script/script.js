0e65adf548556197cde1f9e4fc2cf46019840fec
jade424 <jade424.tn@gmail.com>
2021-12-01 19:52:17 -0500

    oooof

--- script/script.js
+++ script/script.js
@@ -4,4 +4,14 @@
 //Food Pebbles
 //Heart bubbles
 //Rest Button
-
+class Shiba {
+    constructor() {
+        this.name = "";
+        this.age = 0;
+        this.hunger = 0;
+        this.bored = 0;
+        this.isLoved = true;
+        this.sleepy = 0;
+        this.isAlive = true;
+    }
+    
