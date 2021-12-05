9d33ced595b51a331ac1a06c9fdbcafaae202b34
jade424 <jade424.tn@gmail.com>
2021-12-05 17:01:35 -0500

    starting javascript

---
+++ script/script.js
@@ -0,0 +1,9 @@
+//I neeed to set variables
+//I need to set a class for the pet
+//I need to set animation for the images (heart / sushi / ball /etc) to fall in the game and be clickable
+//I need to add the sound effect for when buttons are clicked 
+//I need to create a progress meter to inidcate pet's status
+//I need to set this progress meter to interact with the movements in the game
+//I need to set a naptime/bedtime button (bedtime first ... nap if i have more time to add extra stuff)
+//I need to set a win condition
+//I need to set a lose condition
