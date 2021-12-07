83442d5b7d5d2da5454bf7ba092ceae03c6e3088
jade424 <jade424.tn@gmail.com>
2021-12-06 19:41:36 -0500

    added buttons ... now to make them bigger and functional

--- script/script.js
+++ script/script.js
@@ -7,6 +7,8 @@
 //I need to add the sound effect for when buttons are clicked 
 //I need to create a progress meter to inidcate pet's status
 //I need to set this progress meter to interact with the movements in the game
+
+    function updatePetStat()
 //I need to set a naptime/bedtime button (bedtime first ... nap if i have more time to add extra stuff)
 //I need to set a win condition
 //I need to set a lose condition
