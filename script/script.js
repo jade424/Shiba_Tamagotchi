164044f59b6a9689dc470ea16b7c04e7be0d19f1
jade424 <jade424.tn@gmail.com>
2021-12-07 16:50:22 -0500

    cannot get js to work

--- script/script.js
+++ script/script.js
@@ -1,86 +1,126 @@
-// function petName(name, url) {
-//     if (!url) url = window.location.href;
-//     name = name.replace(/[\[\]]/g, '\\$&');
-//     var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-//         results = regex.exec(url);
-//     if (!results) return null;
-//     if (!results[2]) return '';
-//     return decodeURIComponent(results[2].replace(/\+/g, ' '));
-// } 
-
-// let name = petName('petname');
-
-// document.getElementById("petname").innerHTML = name;
-
-// windows.onload = setTitle;
-
-// window.addEventListener('DOMContentLoaded', (event) => {
-
-    class shiba {
-        constructor() {
-            this.hunger = 0;
-            this.bored = 0;
-            this.sad = 0;
-            this.tired =0;
-
-        }
+    let shiba = {
+        
+        name: " ",
+        hunger: 0,
+        bored: 0,
+        sad: 0,
+        tired: 0,
+        age: 0,
     }
-    let name = " ";
-    let age = 0;
-    let foodScore = 10;
-    let boredomScore = 10; 
-    let fatigueScore = 10; 
 
-    function updatePetStatus(petStatus, value) {
-    hungerStatus = document.getElementById("#hunger__fill").style.width = `$(value)%`;
-    hungerStatus = document.getElementById("#hunger__text").textContent = `$(value)%`;
-    boredStatus = document.getElementById("#bored__fill").style.width = `$(value)%`;
-    boredStatus = document.getElementById("#bored__text").textContent = `$(value)%`;
-    loveStatus = document.getElementById("#sad__fill").style.width = `$(value)%`;
-    loveStatus = document.getElementById("#sad__text").textContent = `$(value)%`;
-    sleepStatus = document.getElementById("#tired__fill").style.width = `$(value)%`;
-    sleepStatus = document.getElementById("#tired__text").textContent = `$(value)%`;
+feed() {
+    this.hunger = this.hunger -1;
+}
+play() {
+    this.bored = this.bored -1;
+}
+love() {
+    this.sad = this.sad -1;
+}
+sleep() {
+    this.tired = this.tired -1;
 }
 
+let $hungerStatus = $('#hungerStatus');
+let $boredStatus = $('#boredStatus');
+let $sadStatus = $('#sadStatus');
+let $sleepStatus = $('#sleepStatus');
+
+$('#start').click(function() {
+    startIntervals();  
+});
+
+let hungerIntervalId = '';
+let boredIntervalId = '';
+let sadIntervalId = '';
+let tiredIntervalId = '';
+
+function clearAllInterval () {
+    clearInterval(hungerIntervalId);
+    clearInterval(boredIntervalId);
+    clearInterval(sadIntervalId);
+    clearInterval(tiredIntervalId);
+}
+
+function startIntervals () {
+    hungerIntervalId = setInterval (function()) {
+        if(shiba.hunger >= 10) {
+            clearInterval(hungerIntervalId);
+            $('#gameover').html('Your shiba has filed for emancipation from starvation')
+            gameOver();
+
+        } else {
+            shiba.hunger = shiba.hunger +1;
+            $hungerStatus.css('width' , shiba.hunger + '%');
+        } 
     
-    let age = 0;
-    updateAge()
+    }
+    boredIntervalId = setInterval (function()) {
+        if(shiba.bored >= 10) {
+            clearInterval(boredIntervalId);
+            $('#gameover').html('Your shiba has filed for emancipation from loneliness')
+            gameOver();
 
-    feedbtn.addEventListener("click", ()=>{
-      hungry__text += 2;
-      if(foodScore > 10) foodScore = 10;
-      updateFoodWidth();
-    });
+        } else {
+            shiba.bored = shiba.bored +1;
+            $boredStatus.css('width' , shiba.bored + '%');
+        } 
+    sadIntervalId = setInterval (function()) {
+        if(shiba.sad >= 10) {
+                clearInterval(sadIntervalId);
+                $('#gameover').html('Your shiba has filed for emancipation from neglect')
+                gameOver();
+    
+        } else {
+                shiba.sad = shiba.sad +1;
+                $sadStatus.css('width' , shiba.sad + '%');
+        } 
+        }
+    }
+    tiredIntervalId = setInterval (function()) {
+        if(shiba.tired >= 10) {
+            clearInterval(tiredIntervalId);
+            $('#gameover').html('Your shiba has filed for emancipation from terrible sleep conditions')
+            gameOver();
 
-    boredbtn.addEventListener("click", ()=>{
-      bored__text += 2;
-      if(boredomScore > 10) boredomScore = 10;
-      updateBoredomWidth();
-    });
-
-    lovebtn.addEventListener("click", ()=>{
-      sad__text += 2;
-      if(fatigueScore > 10) fatigueScore = 10;
-      updateFatigueWidth();
-
-    });
-
-    sleepbtn.addEventListener("click", ()=>{
-        tired__text += 2;
-        if(fatigueScore > 10) fatigueScore = 10;
-        updateFatigueWidth();
-  
-      });
-
-// let shiba {
-//     constructor() {
-//         this.hunger = 0;
-//         this.bored = 0;
-//         this.sad = 0;
-//         this.tired = 0;
-//         isAlive = true;
-//     }
+        } else {
+            shiba.tired = shiba.tired +1;
+            $sleepStatus.css('width' , shiba.tired + '%');
+        } 
 
 
-// }
+    
+    }
+}
+$('#feedbtn').click(function(){
+    shiba.feed();
+});
 
+$('#playbtn').click(function(){
+    shiba.play();
+});
+
+$('#lovebtn').click(function(){
+    shiba.love();
+});
+$('#sleepbtn').click(function(){
+    shiba.sleep();
+});
+
+function petName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+} 
+
+let name = petName('petname');
+
+document.getElementById("petname").innerHTML = name;
+
+windows.onload = setTitle;
+
+window.addEventListener('DOMContentLoaded', (event) => {
