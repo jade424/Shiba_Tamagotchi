949e817c753cfd9786d8418141e7f210d0a30bc2
jade424 <jade424.tn@gmail.com>
2021-12-07 16:09:19 -0500

    ....sigh

--- script/script.js
+++ script/script.js
@@ -1,176 +1,86 @@
-function petName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+// function petName(name, url) {
+//     if (!url) url = window.location.href;
+//     name = name.replace(/[\[\]]/g, '\\$&');
+//     var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+//         results = regex.exec(url);
+//     if (!results) return null;
+//     if (!results[2]) return '';
+//     return decodeURIComponent(results[2].replace(/\+/g, ' '));
+// } 
+
+// let name = petName('petname');
+
+// document.getElementById("petname").innerHTML = name;
+
+// windows.onload = setTitle;
+
+// window.addEventListener('DOMContentLoaded', (event) => {
+
+    class shiba {
+        constructor() {
+            this.hunger = 0;
+            this.bored = 0;
+            this.sad = 0;
+            this.tired =0;
+
+        }
+    }
+    let name = " ";
+    let age = 0;
+    let foodScore = 10;
+    let boredomScore = 10; 
+    let fatigueScore = 10; 
+
+    function updatePetStatus(petStatus, value) {
+    hungerStatus = document.getElementById("#hunger__fill").style.width = `$(value)%`;
+    hungerStatus = document.getElementById("#hunger__text").textContent = `$(value)%`;
+    boredStatus = document.getElementById("#bored__fill").style.width = `$(value)%`;
+    boredStatus = document.getElementById("#bored__text").textContent = `$(value)%`;
+    loveStatus = document.getElementById("#sad__fill").style.width = `$(value)%`;
+    loveStatus = document.getElementById("#sad__text").textContent = `$(value)%`;
+    sleepStatus = document.getElementById("#tired__fill").style.width = `$(value)%`;
+    sleepStatus = document.getElementById("#tired__text").textContent = `$(value)%`;
 }
-let name = petName('petname');
 
-document.getElementById("petname").innerHTML = name;
-
-windows.onload = setTitle;
-
-let shiba = {
-    name: '',
-    hunger: 0,
-    boredom: 0,
-    affection: 10,
-    tired: 0,
     
-    feed() {
-        this.hunger = this.hunger - 1;
-    },
-    play() {
-        this.boredom = this.boredome - 1;
-    },
-    love() {
-        this.affection = this.affection + 1;
-    },
-    sleep() {
-        this.tired = this.tired -1;
-    },
-}
+    let age = 0;
+    updateAge()
 
-let $hungry__fill = $('#hungry__fill');
-let $bored__fill = $('#bored__fill');
-let $sad__fill = $('#sad__fills');
-let $tired__fill = $('#tired__fill');
+    feedbtn.addEventListener("click", ()=>{
+      hungry__text += 2;
+      if(foodScore > 10) foodScore = 10;
+      updateFoodWidth();
+    });
 
-let hungerIntervalId = '';
-let boredomIntervalId = '';
-let affectionIntervalId = '';
-let tiredIntervalId = '';
+    boredbtn.addEventListener("click", ()=>{
+      bored__text += 2;
+      if(boredomScore > 10) boredomScore = 10;
+      updateBoredomWidth();
+    });
 
-let feedSound = new Audio("sounds/zapsplat_catoon_bite_munch_single_002_56564.mp3");
+    lovebtn.addEventListener("click", ()=>{
+      sad__text += 2;
+      if(fatigueScore > 10) fatigueScore = 10;
+      updateFatigueWidth();
 
+    });
 
+    sleepbtn.addEventListener("click", ()=>{
+        tired__text += 2;
+        if(fatigueScore > 10) fatigueScore = 10;
+        updateFatigueWidth();
+  
+      });
 
-// $('#namebutton').click(function () {
-// shiba.name = $("#namebutton").val();
-// // $('#namebox1').html(shiba.name);
-// });
-
-
-//Start Game and Intervals
-
-// function gameOver() {
-//         $('.main').fadeOut();
-//         $('img').fadeOut();
-//         $('.petbox').fadeOut();
-//         $('.end').fadeIn();
-//         clearAllInterval();
-//     knight.hunger = 0;
-//     knight.fatigue = 0;
-//     knight.skill = 100;
-//     knight.age = 1;
-// }
-
-// function restartGame() {
-// setTimeout (() => {
-//     $('.main').fadeIn();
-//     $('img').fadeIn();
-//     $('.petbox').fadeIn();
-//     $('.end').fadeOut();
-// }, 1000)
-// startIntervals();
-// }
-
-// function shine(){
-// $body.css('background-color', '#7a665d');
-// }
-
-// function dimLight(){
-// $body.css('background-color', '#7c142c')
-// }
-
-function clearAllInterval () {
-clearInterval(hungerIntervalId);
-clearInterval(boredomIntervalId);
-clearInterval(affectionntervalId);
-clearInterval(tiredInterval);
-}
-
-function startIntervals() {
-
-hungerIntervalId = setInterval(function() {
-    if(shiba.hunger >= 10) {
-        clearInterval(hungerIntervalId);
-        $('#gameover').html('Your shiba has filed for emancipation from starvation!')
-        gameOver();
-
-    } else {
-        shiba.hunger = shiba.hunger + 3;
-        $hungry__fill.css('width', shiba.hunger + '%');
-    }
-}, 1000);
-
-boredomIntervalId = setInterval(function() {
-    if(shiba.boredom >= 10) {
-        clearInterval(boredomIntervalId);
-        $('#gameover').html('Your shiba has filed for emancipation from lack of excitement!')
-        gameOver();
-        
-    } else {
-        shiba.boredom = shiba.boredom + 1;
-        $bored__fill.css('width', shiba.boredom + '%');
-    }
-}, 1500);
-
-affectionntervalId = setInterval(function() {
-    if(shiba.affection <= 0) {
-        clearInterval(affectionntervalId);
-        $('#gameover').html('Your shiba has filed for emancipation from lack of love!')
-        gameOver();
-        
-    } else {
-        shiba.affection = shiba.affection - 1;
-        $sad__fill.css('width', shiba.affection + '%');
-    }
-}, 3000);
-
-// ageIntervalId = setInterval(function(){
-//     if(knight.hunger >=100 || knight.fatigue >= 100 || knight.skill <= 0) {
-//         clearInterval(ageIntervalId);
-//     }  
-//     else if (knight.age >= 10) {
-//         $('#pet').attr("src", form2);  
-//     }  
-//     else {
-//         knight.age++;
-//         $age.html('Days Survived: ' + knight.age);
+// let shiba {
+//     constructor() {
+//         this.hunger = 0;
+//         this.bored = 0;
+//         this.sad = 0;
+//         this.tired = 0;
+//         isAlive = true;
 //     }
 
-// }, 5000)
 
-
-
-};
-
-
-
-//action buttons
-    $('#feed').click(function(){
-        shiba.feed();
-        feedSound.play();
-    });
-
-    $('#play').click(function(){
-        shiba.play();   
-    });
-
-    $('#love').click(function(){
-        shiba.love();
-    });
-    $('#sleep').click(function() {
-        shiba.sleep();
-    })
-
-    // $('#brighten').click(shine);
-
-    // $('#darken').click(dimLight);
-
-    $('#restart').click(restartGame);
+// }
 
